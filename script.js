3f774f911143a8012ff61335d38eee8c8449a275
daiiiiid <46668629+daiiiiid@users.noreply.github.com>
2019-02-06 17:56:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 $(function() {
+var nav = $('.nav');
+$('li', nav)
+.mouseover(function(e) {
+$('ul', this).stop().slideDown('fast');
+})
+.mouseout(function(e) {
+$('ul', this).stop().slideUp('fast');
+});
+});
 
-  $('.list-item').click(function() {
-    $(this).css('color', 'red');
+  
+  
+  
+$('.list-item').click(function() {
+  $(this).css('color', 'red');
   });
 });
